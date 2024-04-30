c13479d7db70365b55b2d3c9e6b327194b1eb6da
ilham saleh <ilham2saleh@gmail.com>
2024-04-30 12:15:47 +0100

    Clean up games card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -8,17 +8,18 @@ interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
+  console.log(game.released)
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justify="space-between">
+        <HStack justify="space-between" marginBottom={3}>
           <PlatformIcons
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  released: string;
 }
 
 const useGames = (gameQuery: GameQuery) =>
