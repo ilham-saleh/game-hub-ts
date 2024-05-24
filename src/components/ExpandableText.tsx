93339ae219f6056577fe07e414440358e1a670c1
ilham saleh <ilham2saleh@gmail.com>
2024-05-24 10:27:01 +0100

    Create expandable text component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,28 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const textLimit = 300;
+
+  if (children.length <= textLimit) {
+    return <Text>{children}</Text>;
+  }
+
+  const summary = isExpanded ? children : children.substring(0, textLimit);
+
+  return (
+    <Text>
+      {summary}...
+      <Button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "Show less" : "Show more"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,20 +1,32 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetail = () => {
   const { slug } = useParams<{ slug: string }>();
+  // const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-  const { data: game, isLoading, error } = useGame(slug!); // ! mark means this slug is always not a null
+  const { data: game, isLoading, error } = useGame(slug!); // Non-null assertion since slug should be defined
 
   if (isLoading) <Spinner />;
 
   if (error) throw error;
 
+  // const textLimit = 300;
+
+  // const description = game?.description_raw || "";
+  // const isLongText = description.length > textLimit;
+  // const showDescription = isExpanded
+  //   ? description
+  //   : description.substring(0, 300);
+
   return (
     <>
       <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <ExpandableText>
+        {game?.description_raw || "No description available"}
+      </ExpandableText>
     </>
   );
 };
